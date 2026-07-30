6fc483184b3ddb5938516c7ebd2273a9bf75fbc3
phdoerfler <ph@doerfler.io>
2026-07-30 15:15:21 +0200

    feat: add AdventureWorks mapping and join-depth query generator

---
+++ benchmarks-sql/src/main/scala/BenchmarkDb.scala
@@ -0,0 +1,28 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// Copyright (c) 2016-2025 Grackle Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grackle.benchmarks.sql
+
+import cats.effect.Async
+import org.typelevel.doobie.util.transactor.Transactor
+
+object BenchmarkDb {
+  val jdbcUrl: String = "jdbc:postgresql://localhost:5433/benchmark"
+  val username: String = "benchmark"
+  val password: String = "benchmark"
+
+  def transactor[F[_]: Async]: Transactor[F] =
+    Transactor.fromDriverManager[F]("org.postgresql.Driver", jdbcUrl, username, password, None)
+}
---
+++ benchmarks-sql/src/main/scala/JoinChain.scala
@@ -0,0 +1,64 @@
+// Copyright (c) 2016-2025 Association of Universities for Research in Astronomy, Inc. (AURA)
+// Copyright (c) 2016-2025 Grackle Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package grackle.benchmarks.sql
+
+object JoinChain {
+
+  // GraphQL field name for each hop, root (CountryRegion) to leaf (ProductCategory).
+  // Must match the field names in AdventureWorksMapping's schema exactly.
+  val hops: List[String] =
+    List(
+      "stateProvinces",
+      "addresses",
+      "businessEntityAddresses",
+      "person",
+      "customers",
+      "salesOrders",
+      "lineItems",
+      "product",
+      "subcategory",
+      "category"
+    )
+
+  val maxDepth: Int = hops.length
+
+  private val leafField: Map[String, String] =
+    Map(
+      "stateProvinces" -> "name",
+      "addresses" -> "city",
+      "businessEntityAddresses" -> "addressTypeId",
+      "person" -> "lastName",
+      "customers" -> "accountNumber",
+      "salesOrders" -> "salesOrderNumber",
+      "lineItems" -> "unitPrice",
+      "product" -> "name",
+      "subcategory" -> "name",
+      "category" -> "name"
+    )
+
+  def queryForDepth(depth: Int): String = {
+    require(depth >= 1 && depth <= maxDepth, s"depth must be between 1 and $maxDepth, got $depth")
+
+    def nest(remaining: List[String]): String =
+      remaining match {
+        case field :: Nil => s"$field { ${leafField(field)} }"
+        case field :: rest => s"$field { ${nest(rest)} }"
+        case Nil => throw new IllegalStateException("unreachable: depth bounds checked above")
+      }
+
+    s"query { countryRegions { countryRegionCode ${nest(hops.take(depth))} } }"
+  }
+}
